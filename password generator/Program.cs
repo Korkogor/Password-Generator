aed52c0a42e313526c491cad402703664fbf0a09
Daniil <korkogor@gmail.com>
2025-05-23 14:07:48 +0300

    Password Generator

---
+++ password generator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+static int GetPasswordLenght()
+{
+    Console.WriteLine("Введите длину пароля: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+static string GeneratePassword(int lenght)
+{
+    const string chars =
+        "abcdefghijklmnopqrstuvwxyz" +
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+        "0123456789!@#$%^&*";
+
+    var random = new Random();
+    var result = new StringBuilder();
+
+        for (int i = 0; i < lenght; i++)    
+        result.Append(chars[random.Next(chars.Length)]);
+        return result.ToString();
+    
+}
+
+static void Main()
+{
+    int length = GetPasswordLenght();
+    string password = GeneratePassword(length);
+    
+    Console.WriteLine($"Ваш случайный пароль: {password}");
+}
